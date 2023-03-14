1134e3dfe8c91f25913d8d5decaf96080563a09f
Pradeesh P <79352210+PRADEESH101@users.noreply.github.com>
2023-03-14 09:56:00 +0530

    14.03.2023

--- src/index.js
+++ src/index.js
@@ -1,20 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// // root.render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// // );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // reportWebVitals();
 
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element, document.getElementById('root'));;
+// const element = <h1>Hello World</h1>;
+// ReactDOM.render(element, document.getElementById('root'));;
+
+// function sayhello(){
+//     for(let i =0; i<5;i++){
+//         console.log(i);
+//     }
+// }
+
+// sayhello();
+
+// const person = {
+//     name: 'pradeesh',
+//     walk(){
+//         console.log(this);
+//     }
+
+// };
+
+// person.walk();
+
+// const walk = person.walk.bind(person);
+// walk();
+// const targetMember = 'name';
+// person[targetMember.value] = 'john';
+
+// const squre = function(number){
+//     return number * number;
+// }
+// const square = number => number * number;
+// console.log(square(5));
+
+// const jobs =[
+//     {id:1, isActive: true},
+//     {id:2, isActive: true},
+//     {id:3, isActive: false},
+
+// ];
+
+// const activeJobs = jobs.filter(function(job) { return job.isActive;});
